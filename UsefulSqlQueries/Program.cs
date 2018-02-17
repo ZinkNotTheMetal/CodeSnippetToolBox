aa25cace1ea554050de1cd398f59e88a3cea88d1
William Zink <williamdzink@gmail.com>
2018-02-16 23:06:37 -0600

    Upgraded nuget packages Upgraded .net version to 4.7.1 Added program.cs to allow proper build

---
+++ UsefulSqlQueries/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace UsefulSqlQueries
+{
+    //This class just enables the build to compile correctly
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
